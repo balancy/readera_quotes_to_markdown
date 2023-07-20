04d51037d1e914b8f840938c76cf7343a6cf4116
balancy <balancy@gmail.com>
2023-07-20 18:01:22 +0200

    Add parsing arguments such as input and output file

--- format_quotes.py
+++ format_quotes.py
@@ -1,3 +1,6 @@
+import argparse
+import logging
+import os
 from typing import TextIO
 
 
@@ -10,12 +13,12 @@ class QuotesFormatter:
         self._is_line_quote: bool = True
         self._is_line_comment: bool = False
 
-    def _read_headers(self, quotes_file: TextIO):
-        title = quotes_file.readline()
-        author = quotes_file.readline()
+    def _read_headers(self, quotes_file: TextIO) -> None:
+        title: str = quotes_file.readline()
+        author: str = quotes_file.readline()
         self._markdown = f'# {title}\n' f'## {author}\n'
 
-    def _process_five_stars(self):
+    def _process_five_stars(self) -> None:
         if self._is_line_quote:
             self._markdown += f'{self._current_quote}\n'
             self._current_quote = ''
@@ -28,20 +31,23 @@ class QuotesFormatter:
 
         self._markdown += '***\n'
 
-    def _process_two_hypens(self):
+    def _process_two_hypens(self) -> None:
         self._markdown += f'{self._current_quote}\n'
         self._current_quote = ''
 
         self._is_line_quote = False
         self._is_line_comment = True
 
-    def _process_text(self, text: str):
+    def _process_text(self, text: str) -> None:
         if self._is_line_quote:
             self._current_quote += f'> {text}\n'
         elif self._is_line_comment:
             self._current_comment += f'{text}\n'
 
-    def process(self):
+    def process(self) -> str:
+        if not os.path.exists(self._quotes_filename):
+            raise FileNotFoundError
+
         with open(self._quotes_filename, 'r') as quotes_file:
             self._read_headers(quotes_file)
 
@@ -58,8 +64,43 @@ class QuotesFormatter:
         return self._markdown
 
 
+def parse_arguments() -> tuple[str, str]:
+    parser = argparse.ArgumentParser(
+        prog='Script to format quotes from a text file to markdown',
+        description='Main usage is to export ReadEra quotes to Notion',
+    )
+
+    parser.add_argument(
+        '--input_file',
+        '-i',
+        type=str,
+        default='quotes.txt',
+        help='Name of the input file with quotes. By default "quotes.txt"',
+    )
+    parser.add_argument(
+        '--output_file',
+        '-o',
+        type=str,
+        default='quotes.md',
+        help='Name of the output file with quotes. By default "quotes.md"',
+    )
+
+    args = parser.parse_args()
+    quotes_file: str = args.input_file
+    quotes_md: str = args.output_file
+
+    return quotes_file, quotes_md
+
+
 if __name__ == "__main__":
-    quotes_formatter = QuotesFormatter('quotes.txt')
-    markdown = quotes_formatter.process()
-    with open('quotes.md', 'w') as quotes_md:
-        quotes_md.write(markdown)
+    quotes_file, quotes_md = parse_arguments()
+
+    quotes_formatter = QuotesFormatter(quotes_file)
+
+    try:
+        markdown = quotes_formatter.process()
+    except FileNotFoundError:
+        logging.error(f'File {quotes_file} not found')
+    else:
+        with open(quotes_md, 'w') as quotes_md:
+            quotes_md.write(markdown)
